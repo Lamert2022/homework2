0b8339e87891151100433475ec094a3a43c625d3
Dmitriy <118912417+Lamert2022@users.noreply.github.com>
2022-12-09 09:36:30 +0300

    first commit

---
+++ homework13/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.Write("Введите число: ");
+var str = Console.ReadLine();
+
+if (!int.TryParse(str, out int num))
+{
+    Console.WriteLine("Не является числом");
+    return;
+};
+
+num = Math.Abs(num);
+if (num < 100)
+{
+    Console.WriteLine("В числе нет третьей цифры");
+    return;
+}
+
+Console.WriteLine($"Третья цифра числа {str}: {num.ToString()[2]}");
---
+++ homework15/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.Write("Введите номер дня недели: ");
+var str = Console.ReadLine();
+
+if (!int.TryParse(str, out int num))
+{
+    Console.WriteLine("Не является числом");
+    return;
+};
+
+switch (num)
+{
+    case 6:
+        Console.WriteLine("Выходной");
+        break;
+    case 7:
+        Console.WriteLine("Выходной");
+        break;
+    default:
+        Console.WriteLine("Будний день");
+        break;
+}
